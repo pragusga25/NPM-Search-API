7577e0f05ef35af9b5b879bc205436470709b06b
Taufik Pragusga <pragusga25@gmail.com>
2021-04-20 22:14:53 +0700

    Consuming store state

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -21,6 +21,13 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error &&
+        !loading &&
+        data.map((name, index) => {
+          return <div key={index}>{name}</div>;
+        })}
     </div>
   );
 };
