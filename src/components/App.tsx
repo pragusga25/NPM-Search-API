e0034f46d0047e54857b616fee2ed617b20ab8ad
Taufik Pragusga <73622464+pragusga25@users.noreply.github.com>
2021-06-10 16:10:50 +0700

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../state';
 import RepositoriesList from './RepositoriesList';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div>
